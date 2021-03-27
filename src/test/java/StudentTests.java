2286e6ef146cbec95786752abdc196098fc52009
vladvlad23 <ungureanu.vlad1@gmail.com>
2021-03-27 16:34:02 +0200

    new changes

--- src/test/java/StudentTests.java
+++ src/test/java/StudentTests.java
@@ -7,8 +7,6 @@ import repository.NotaXMLRepository;
 import repository.StudentXMLRepository;
 import repository.TemaXMLRepository;
 import service.Service;
-import validation.ValidationException;
-import validation.Validator;
 
 import static org.junit.Assert.assertEquals;
 
@@ -22,6 +20,12 @@ public class StudentTests {
 
     Service testService;
 
+    public static final String VALID_ID = "999";
+    public static final String VALID_NAME = "valid";
+    public static final int VALID_GROUP = 512;
+    public static final int SUCCESS_RETURN_VALUE = 1;
+    public static final int FAILURE_RETURN_VALUE = 0;
+
 
     @Before
     public void init() {
@@ -29,21 +33,28 @@ public class StudentTests {
         testService = new Service(studentXmlRepo, temaXMLRepository, notaXMLRepository);
     }
 
+    @Test
+    public void whenAddStudent_CorrectParameters_AdditionSucceeds(){
+        Student student = new Student(VALID_ID, VALID_NAME, VALID_GROUP);
+   //     when(studentXmlRepo.save(student)).thenReturn(student);
+        assertEquals(testService.saveStudent(VALID_ID,VALID_NAME,VALID_GROUP),FAILURE_RETURN_VALUE);
+    }
+
 
     @Test
     public void whenAddStudent_WithEmptyName_AdditionFails(){
-        assertEquals(testService.saveStudent("999","",999),1);
-        assertEquals(testService.saveStudent("999",null,999),1);
+        assertEquals(testService.saveStudent(VALID_ID,"",999),SUCCESS_RETURN_VALUE);
+        assertEquals(testService.saveStudent(VALID_ID,null,999),SUCCESS_RETURN_VALUE);
     }
 
     @Test
     public void whenAddStudent_WithInvalidGroup_AdditionFails(){
-        assertEquals(testService.saveStudent("999","valid",999),1);
-        assertEquals(testService.saveStudent("999","valid",99),1);
+        assertEquals(testService.saveStudent(VALID_ID,VALID_NAME,999),SUCCESS_RETURN_VALUE);
+        assertEquals(testService.saveStudent(VALID_ID,VALID_NAME,99),SUCCESS_RETURN_VALUE);
     }
 
     @Test
     public void whenAddStudent_WithNoId_AdditionFails() {
-        assertEquals(testService.saveStudent(null, "", 999), 1);
+        assertEquals(testService.saveStudent(null, VALID_NAME, 999), SUCCESS_RETURN_VALUE);
     }
 }

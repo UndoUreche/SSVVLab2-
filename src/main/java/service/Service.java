d868157dd0b8a893cc818614fa213a35d32c9475
vladvlad23 <ungureanu.vlad1@gmail.com>
2021-04-03 16:42:11 +0300

    Added assignment add tests and fixed some code.

--- src/main/java/service/Service.java
+++ src/main/java/service/Service.java
@@ -2,6 +2,7 @@ package service;
 
 import domain.*;
 import repository.*;
+import validation.ValidationException;
 
 import java.time.LocalDate;
 import java.time.temporal.WeekFields;
@@ -36,12 +37,17 @@ public class Service {
 
     public int saveTema(String id, String descriere, int deadline, int startline) {
         Tema tema = new Tema(id, descriere, deadline, startline);
-        Tema result = temaXmlRepo.save(tema);
+        try {
+            Tema result = temaXmlRepo.save(tema);
 
-        if (result == null) {
-            return 1;
+            if (result == null) {
+                return 1;
+            }
+            return 0;
         }
-        return 0;
+        catch(ValidationException ex){
+            return 0;
+        }
     }
 
     public int saveNota(String idStudent, String idTema, double valNota, int predata, String feedback) {
--- src/test/java/AssignmentTests.java
+++ src/test/java/AssignmentTests.java
@@ -7,8 +7,14 @@ import repository.NotaXMLRepository;
 import repository.StudentXMLRepository;
 import repository.TemaXMLRepository;
 import service.Service;
+import validation.TemaValidator;
+import validation.ValidationException;
+
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.when;
 
 public class AssignmentTests {
@@ -17,6 +23,8 @@ public class AssignmentTests {
     public static final String VALID_DESCRIPTION = "description";
     public static final int VALID_STARTLINE = 1;
     public static final int VALID_DEADLINE = 4;
+    public static final int MAX_DEADLINE = 14;
+    public static final int MIN_DEADLINE = 1;
 
     @Mock
     StudentXMLRepository studentXmlRepo;
@@ -26,25 +34,129 @@ public class AssignmentTests {
     NotaXMLRepository notaXMLRepository;
 
     Service testService;
+    TemaValidator temaValidator;
 
     @Before
-    public void init(){
+    public void init() {
         MockitoAnnotations.initMocks(this);
         testService = new Service(studentXmlRepo, temaXMLRepository, notaXMLRepository);
+        temaValidator = new TemaValidator();
+        try {
+            PrintWriter testPrintWriter = new PrintWriter("testXML.xml");
+            testPrintWriter.print("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n");
+            testPrintWriter.print("<Entitati>\n");
+            testPrintWriter.print("</Entitati>\n");
+            testPrintWriter.close();
+
+        } catch (IOException e) {
+            System.out.println("Error when initiating test xml files");
+        }
     }
 
     @Test
-    public void whenAddAssignment_CorrectParameters_AdditionSucceeds(){
-        Tema entity = new Tema(VALID_ID,VALID_DESCRIPTION,VALID_STARTLINE,VALID_DEADLINE);
+    public void whenAddAssignment_CorrectParameters_AdditionSucceeds() {
+        Tema entity = new Tema(VALID_ID, VALID_DESCRIPTION, VALID_STARTLINE, VALID_DEADLINE);
         when(temaXMLRepository.save(entity)).thenReturn(null);
-        assertEquals(testService.saveTema(VALID_ID,VALID_DESCRIPTION,VALID_STARTLINE,VALID_DEADLINE),1);
+        assertEquals(testService.saveTema(VALID_ID, VALID_DESCRIPTION, VALID_STARTLINE, VALID_DEADLINE), 1);
     }
 
     @Test
-    public void whenAddAssignment_IdenticalWithExisting_AdditionFails(){
-        Tema entity = new Tema(VALID_ID,VALID_DESCRIPTION,VALID_STARTLINE,VALID_DEADLINE);
+    public void whenAddAssignment_IdenticalWithExisting_AdditionFails() {
+        Tema entity = new Tema(VALID_ID, VALID_DESCRIPTION, VALID_STARTLINE, VALID_DEADLINE);
         when(temaXMLRepository.save(entity)).thenReturn(entity);
-        assertEquals(testService.saveTema(VALID_ID,VALID_DESCRIPTION,VALID_STARTLINE,VALID_DEADLINE), 0);
+        assertEquals(testService.saveTema(VALID_ID, VALID_DESCRIPTION, VALID_STARTLINE, VALID_DEADLINE), 0);
+    }
+
+
+    @Test
+    public void whenAddAssignmentToXMLRepo_CorrectParameters_AdditionSucceeds() {
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, "testXML.xml");
+        Tema entity = new Tema(VALID_ID, VALID_DESCRIPTION, VALID_DEADLINE, VALID_STARTLINE);
+        assertEquals(null, temaXMLRepository.save(entity));
+    }
+
+    @Test
+    public void whenAddAssignmentToXMLRepo_InvalidId_AdditionFails() {
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, "testXML.xml");
+        Tema entity = new Tema("", VALID_DESCRIPTION, VALID_DEADLINE, VALID_STARTLINE);
+        assertThrows(ValidationException.class, () -> {
+            temaXMLRepository.save(entity);
+        });
+    }
+
+    @Test
+    public void whenAddAssignmentToXMLRepo_NullId_AdditionFails() {
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, "testXML.xml");
+        Tema entity = new Tema(null, VALID_DESCRIPTION, VALID_DEADLINE, VALID_STARTLINE);
+        assertThrows(ValidationException.class, () -> {
+            temaXMLRepository.save(entity);
+        });
+    }
+
+    @Test
+    public void whenAddAssignmentToXMLRepo_InvalidDescription_AdditionFails() {
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, "testXML.xml");
+        Tema entity = new Tema(VALID_ID, "", VALID_DEADLINE, VALID_STARTLINE);
+        assertThrows(ValidationException.class, () -> {
+            temaXMLRepository.save(entity);
+        });
+    }
+
+    @Test
+    public void whenAddAssignmentToXMLRepo_NullDescription_AdditionFails() {
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, "testXML.xml");
+        Tema entity = new Tema(VALID_ID, null, VALID_DEADLINE, VALID_STARTLINE);
+        assertThrows(ValidationException.class, () -> {
+            temaXMLRepository.save(entity);
+        });
+    }
+
+    @Test
+    public void whenAddAssignmentToXMLRepo_DeadlineBeforeMin_AdditionFails() {
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, "testXML.xml");
+        Tema entity = new Tema(VALID_ID, VALID_DESCRIPTION, MIN_DEADLINE - 1, VALID_STARTLINE);
+        assertThrows(ValidationException.class, () -> {
+            temaXMLRepository.save(entity);
+        });
+    }
+
+
+    @Test
+    public void whenAddAssignmentToXMLRepo_DeadlineAfterMax_AdditionFails() {
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, "testXML.xml");
+        Tema entity = new Tema(VALID_ID, VALID_DESCRIPTION, MAX_DEADLINE + 1, VALID_STARTLINE);
+        assertThrows(ValidationException.class, () -> {
+            temaXMLRepository.save(entity);
+        });
+    }
+
+
+    @Test
+    public void whenAddAssignmentToXMLRepo_DeadlineBeforeStartline_AdditionFails() {
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, "testXML.xml");
+        //DEADLINE AND STARTLINE INVERSED
+        Tema entity = new Tema(VALID_ID, VALID_DESCRIPTION, VALID_STARTLINE, VALID_DEADLINE);
+        assertThrows(ValidationException.class, () -> {
+            temaXMLRepository.save(entity);
+        });
+    }
+
+    @Test
+    public void whenAddAssignmentToXMLRepo_StartlineBeforeMin_AdditionFails() {
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, "testXML.xml");
+        Tema entity = new Tema(VALID_ID, VALID_DESCRIPTION, VALID_DEADLINE, MIN_DEADLINE - 1);
+        assertThrows(ValidationException.class, () -> {
+            temaXMLRepository.save(entity);
+        });
+    }
+
+    @Test
+    public void whenAddAssignmentToXMLRepo_StartlineAfterMax_AdditionFails() {
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, "testXML.xml");
+        Tema entity = new Tema(VALID_ID, VALID_DESCRIPTION, VALID_DEADLINE, MAX_DEADLINE + 1);
+        assertThrows(ValidationException.class, () -> {
+            temaXMLRepository.save(entity);
+        });
     }
 
 
